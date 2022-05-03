e3afa9bf0ba527ffbe485ab3604ad18a405308ec
Eduardo Ortega <rotjeot@gmail.com>
2022-05-02 22:46:21 -0500

    Agregado boton play y función de aparecer dibujos y desaparecer mensaje central

--- app/src/main/java/com/rotjeot/lizardspock/MainActivity.java
+++ app/src/main/java/com/rotjeot/lizardspock/MainActivity.java
@@ -3,6 +3,11 @@ package com.rotjeot.lizardspock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.playButton);
+        //Imagenes, juego de la computadora
+        ImageView rockImg = (ImageView) findViewById(R.id.rockImage);
+        ImageView papperImg = (ImageView) findViewById(R.id.papperImage);
+        ImageView scissorsImg = (ImageView) findViewById(R.id.scissorsImage);
+        ImageView lizardImg = (ImageView) findViewById(R.id.lizardImage);
+        ImageView spockImg = (ImageView) findViewById(R.id.spockImage);
+        //Botones, juego usuario
+        ImageButton rockBtn = (ImageButton) findViewById(R.id.rockButton);
+        ImageButton papperBtn = (ImageButton) findViewById(R.id.papperButton);
+        ImageButton scissorsBtn = (ImageButton) findViewById(R.id.scissorsButton);
+        ImageButton lizardBtn = (ImageButton) findViewById(R.id.lizardButton);
+        ImageButton spockBtn = (ImageButton) findViewById(R.id.spockButton);
+        //Texto
+        TextView mensaje = (TextView) findViewById(R.id.mensaje);
+
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rockImg.setVisibility(View.VISIBLE);
+                papperImg.setVisibility(View.VISIBLE);
+                scissorsImg.setVisibility(View.VISIBLE);
+                lizardImg.setVisibility(View.VISIBLE);
+                spockImg.setVisibility(View.VISIBLE);
+
+                rockBtn.setVisibility(View.VISIBLE);
+                papperBtn.setVisibility(View.VISIBLE);
+                scissorsBtn.setVisibility(View.VISIBLE);
+                lizardBtn.setVisibility(View.VISIBLE);
+                spockBtn.setVisibility(View.VISIBLE);
+
+                mensaje.setVisibility(View.INVISIBLE);
+
+
+            }
+        });
+
+
     }
 }
